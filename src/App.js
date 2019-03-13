24826945fed1fa4027c2e12646cc2fd1cb76290d
Karol Gucwa <karolg88@gmail.com>
2019-03-13 20:03:27 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Test
-      </div>
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
     );
   }
 }
