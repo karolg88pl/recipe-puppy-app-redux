520b7ac81d6e17277a2efa8b1a2d06e5743820cf
Karol Gucwa <karolg88@gmail.com>
2019-03-13 20:20:06 +0100

    routing imlemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import MainPage from './components/MainPage';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <React.Fragment>
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+        </Switch>
       </React.Fragment>
+      </Router>
     );
   }
 }
