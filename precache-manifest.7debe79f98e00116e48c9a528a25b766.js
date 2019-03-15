bc320111889dc5a8cf05f8c5c47d3a11b40f4cc1
Karol Gucwa <karolg88@gmail.com>
2019-03-15 16:17:56 +0100

    Updates

---
+++ precache-manifest.7debe79f98e00116e48c9a528a25b766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fddf24c1b1b2b692942910c62fc10e89",
+    "url": "/recipe-puppy-app-redux/static/media/no_results.fddf24c1.jpg"
+  },
+  {
+    "revision": "aab3798198a49c7c99ea6b652a67174b",
+    "url": "/recipe-puppy-app-redux/static/media/cook_avatar.aab37981.png"
+  },
+  {
+    "revision": "8a87a01a0186e3bb154d",
+    "url": "/recipe-puppy-app-redux/static/js/runtime~main.8fcc1568.js"
+  },
+  {
+    "revision": "4790283d05dcf69efa02",
+    "url": "/recipe-puppy-app-redux/static/js/main.fab93bed.chunk.js"
+  },
+  {
+    "revision": "15a1ab0f4646646ad70b",
+    "url": "/recipe-puppy-app-redux/static/js/2.17cdbcbd.chunk.js"
+  },
+  {
+    "revision": "45c8c8413869820d2a9d6a76c59dd4be",
+    "url": "/recipe-puppy-app-redux/index.html"
+  }
+];
